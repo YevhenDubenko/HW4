cb64755fda1fd5ed1e899ad39482726c715904c7
YevhenDubenko <74934934+YevhenDubenko@users.noreply.github.com>
2020-12-01 12:13:36 +0200

    Task 4  The math quiz program
    Write a program that asks the answer for a mathematical expression, checks whether the user is right or wrong, and then responds with a message accordingly.

---
+++ task4.py
@@ -0,0 +1,20 @@
+a = int(input("2 + 2 = "))
+if a == 4:
+    print("Good,boy")
+else:
+    print("Try again")
+b = int(input("5 * 2 = "))
+if b == 10:
+    print("Good,boy")
+else:
+    print("Try again")
+c = int(input("12 / 4 = "))
+if c == 3:
+    print("Good,boy")
+else:
+    print("Try again")
+d = int(input("12 - 4 = "))
+if d == 8:
+    print("Good,boy")
+else:
+    print("Try again")
